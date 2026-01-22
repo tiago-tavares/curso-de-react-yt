b83f4fe7e5735203a2b973ce4adbbdd012295ae1
Tiago Avoleta Tavares <tiago.tavares@amil.com.br>
2026-01-22 09:21:34 -0300

    correção do nome do import Task

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import AddTask from "./components/AddTask";
-import Tasks from "./components/tasks";
+import Tasks from "./components/Tasks";
 import { v4 } from "uuid";
 import Title from "./components/Title";
 
